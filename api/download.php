a2c9607da4fced22b4e70cb2c3d82f0ad787dc1e
Ashish Doneriya <ashishdoneriya@gmail.com>
2017-12-28 01:20:05 +0530

    Added files listing feature

---
+++ api/download.php
@@ -0,0 +1,10 @@
+<?php
+    header("Pragma: public"); // required
+    header("Expires: 0");
+    header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+    $path = $_GET['path'];
+    $basename = basename($path);
+    header('Content-Disposition: attachment; filename="' . $basename . '"');
+    header("Content-Length: ". filesize($path));
+    readfile($path);
+?>
