248433ef5dfbe57e4d84441888374b6682df700e
Ashish Doneriya <ashishdoneriya@gmail.com>
2018-01-30 01:24:16 +0530

    Added delete option

---
+++ apis/delete.php
@@ -0,0 +1,31 @@
+<?php
+include_once './base-dir.php';
+
+header("Access-Control-Allow-Methods: POST");
+$data = json_decode(file_get_contents('php://input'), true);
+$parentDir = htmlspecialchars(strip_tags($data['parentDir']));
+$file = htmlspecialchars(strip_tags($data['file']));
+
+if (strpos($parentDir, '/') != 0) {
+	$parentDir =  '/' . $parentDir;
+}
+$parentDir = $baseDir . $parentDir;
+
+if (strpos($parentDir, '/./') != false || strpos($parentDir, '..') != false) {
+	return;
+}
+
+$fullPath = $parentDir . '/' . $file;
+
+if ($fullPath == $baseDir . '/' || $fullPath == $baseDir) {
+	return;
+}
+
+if (is_dir($fullPath)) {
+	shell_exec('rmdir "' . $fullPath . '"');
+} else {
+	shell_exec('rm "' . $fullPath . '"');
+}
+
+echo 'success';
+?>
