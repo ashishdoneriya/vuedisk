cfab4d6d51128fc8840804685cbc78b34a968274
Ashish Doneriya <ashishdoneriya@gmail.com>
2018-01-29 10:21:07 +0530

    .

--- apis/remote-upload.php
+++ apis/remote-upload.php
@@ -3,12 +3,24 @@ include_once './base-dir.php';
 
 header("Access-Control-Allow-Methods: POST");
 $data = json_decode(file_get_contents('php://input'), true);
-$currentDir = htmlspecialchars(strip_tags($data['currentDir']));
+$parentDir = htmlspecialchars(strip_tags($data['parentDir']));
 $url = htmlspecialchars(strip_tags($data['url']));
 $filename = htmlspecialchars(strip_tags($data['filename']));
 if ($filename == null || $filename == '') {
 	$filename = basename($url);
 }
-copy($url, $currentDir . '/' . $filename);
+copy($url, $parentDir . '/' . $filename);
+if (strpos($parentDir, '/') != 0) {
+	$parentDir =  '/' . $parentDir;
+}
+$parentDir = $baseDir . $parentDir;
+
+if (strpos($parentDir, '/./') != false || strpos($parentDir, '..') != false) {
+	echo "error";
+	return;
+}
+
+shell_exec('wget $url -O $filename -P $parentDir');
+
 echo "success";
 ?>
