981accdbf8fcbf0a4b01b3aea5b2a0b2a232a2a2
Ashish Doneriya <ashishdoneriya@gmail.com>
2018-03-19 16:29:42 +0530

    Create signout.php

---
+++ apis/signout.php
@@ -0,0 +1,18 @@
+<?php
+header("Access-Control-Allow-Methods: POST");
+session_start();
+// remove all session variables
+session_unset();
+// If it's desired to kill the session, also delete the session cookie.
+// Note: This will destroy the session, and not just the session data!
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+// destroy the session
+session_destroy();
+echo 'success';
+?>
