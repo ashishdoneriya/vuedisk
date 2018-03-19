02a6eb9d93eb54159b62f4ce0c8b9a950cd8b5b8
Ashish Doneriya <ashishdoneriya@gmail.com>
2018-03-19 16:26:28 +0530

    Update base-dir.php

--- apis/base-dir.php
+++ apis/base-dir.php
@@ -1,4 +1,28 @@
 <?php
-	$baseDir = "/home/ashish";
+	$baseDir = "/home/impadmin";
+	$globalPassword = "pass@123";
 	// Note : don't add '/' at the end
+	
+	
+function isSessionActive() {
+	session_start();
+	if (isset($_SESSION['LAST_ACTIVITY']) && ($_SESSION['LAST_ACTIVITY'] + 86400) > time()) {
+		return true;
+	} else {
+		// remove all session variables
+	session_unset();
+		// If it's desired to kill the session, also delete the session cookie.
+		// Note: This will destroy the session, and not just the session data!
+		if (ini_get("session.use_cookies")) {
+		    $params = session_get_cookie_params();
+		    setcookie(session_name(), '', time() - 42000,
+		        $params["path"], $params["domain"],
+		        $params["secure"], $params["httponly"]
+		    );
+		}
+		// destroy the session
+		session_destroy();
+		return false;
+	}
+}
 ?>
