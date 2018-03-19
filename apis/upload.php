77f92b20bece26367f5faf963f64b5eedeb8abff
Ashish Doneriya <ashishdoneriya@gmail.com>
2018-03-19 16:30:15 +0530

    Update upload.php

--- apis/upload.php
+++ apis/upload.php
@@ -1,6 +1,11 @@
 
 <?php
 
+if (!isSessionActive()) {
+	echo '{"status" : "failed", "message" : "Login Required"}';
+	return;
+}
+
 include_once './base-dir.php';
 // In PHP versions earlier than 4.1.0, $HTTP_POST_FILES should be used instead
 // of $_FILES.
