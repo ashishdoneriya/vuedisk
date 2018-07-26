55fbcdf348011e86f35eadc0a281515f2205fddb
Ashish Doneriya <ashishdoneriya@gmail.com>
2018-07-26 23:07:46 +0530

    Fixed larger files downloading

--- apis/download.php
+++ apis/download.php
@@ -14,11 +14,29 @@ if (strpos($path, '/') != 0) {
 	$path =  '/' . $path;
 }
 $path = $baseDir . $path;
-$basename = basename($path);
-header("Pragma: public"); // required
-header("Expires: 0");
-header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-header('Content-Disposition: attachment; filename="' . $basename . '"');
-header("Content-Length: ". filesize($path));
-readfile($path);
+$chunksize = 5 * (1024 * 1024); //5 MB (= 5 242 880 bytes) per one chunk of file.
+
+if (file_exists($path)) {
+    set_time_limit(300);
+    $size = intval(sprintf("%u", filesize($path)));
+    header('Content-Type: application/octet-stream');
+    header('Content-Transfer-Encoding: binary');
+    header('Content-Length: '.$size);
+    header('Content-Disposition: attachment;filename="'.basename($path).'"');
+
+    if ($size > $chunksize) {
+        $handle = fopen($path, 'rb');
+        while (!feof($handle)) {
+          print(@fread($handle, $chunksize));
+          ob_flush();
+          flush();
+        }
+        fclose($handle);
+		} else {
+			readfile($path);
+		}
+		exit;
+} else {
+	echo 'File "'.$filename.'" does not exist!';
+}
 ?>
