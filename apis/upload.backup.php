3e88b1e0855f522c762c1d3ddee8cf136920ba95
Ashish Doneriya <ashishdoneriya@gmail.com>
2019-04-11 02:49:36 +0530

    Added base code to improve uploading speed

---
+++ apis/upload.backup.php
@@ -0,0 +1,68 @@
+
+<?php
+include_once './base-dir.php';
+
+$baseDir = getBaseDirectory();
+if ($baseDir == null) {
+	http_response_code(401);
+	echo '{"status" : "failed", "message" : "Login Required"}';
+	return;
+}
+
+$parentDir = getParam('parentDir');
+
+if (!ends_with($parentDir, '/')) {
+	$parentDir = $parentDir . '/';
+}
+
+if (strpos($parentDir, '/') != 0) {
+	$parentDir =  '/' . $parentDir;
+}
+
+$parentDir = $baseDir . $parentDir . '/';
+
+$uploadfilePath = $parentDir . str_replace(' ', ' ', basename($_FILES['file']['name']));
+if (rename($_FILES['file']['tmp_name'], $uploadfilePath)) {
+	echo "success";
+} else {
+	echo "error";
+}
+//unlink($_FILES['file']['tmp_name']);
+
+
+// In PHP versions earlier than 4.1.0, $HTTP_POST_FILES should be used instead
+// of $_FILES.
+
+/**
+ * Get the given variable from $_REQUEST or from the url
+ * @param string $variableName
+ * @param mixed $default
+ * @return mixed|null
+ */
+function getParam($variableName, $default = null) {
+
+	// Was the variable actually part of the request
+	if (array_key_exists($variableName, $_REQUEST)) {
+		return $_REQUEST[$variableName];
+	}
+
+	// Was the variable part of the url
+	$urlParts = explode('/', preg_replace('/\?.+/', '', $_SERVER['REQUEST_URI']));
+	$position = array_search($variableName, $urlParts);
+	if ($position !== false && array_key_exists($position + 1, $urlParts)) {
+		return $urlParts[$position + 1];
+	}
+
+	return $default;
+}
+
+function ends_with($haystack, $needles) {
+	foreach ((array) $needles as $needle) {
+		if ((string) $needle === substr($haystack, -strlen($needle))) {
+			return true;
+		}
+	}
+	return false;
+}
+
+?>
--- apis/upload.php
+++ apis/upload.php
@@ -1,5 +1,5 @@
-
 <?php
+
 include_once './base-dir.php';
 
 $baseDir = getBaseDirectory();
@@ -9,7 +9,16 @@ if ($baseDir == null) {
 	return;
 }
 
-$parentDir = getParam('parentDir');
+$parentDir = $_POST['parentDir'];
+
+function ends_with($haystack, $needles) {
+	foreach ((array) $needles as $needle) {
+		if ((string) $needle === substr($haystack, -strlen($needle))) {
+			return true;
+		}
+	}
+	return false;
+}
 
 if (!ends_with($parentDir, '/')) {
 	$parentDir = $parentDir . '/';
@@ -20,50 +29,124 @@ if (strpos($parentDir, '/') != 0) {
 }
 
 $parentDir = $baseDir . $parentDir . '/';
+$num = $_POST['num'];
 
-$uploadfilePath = $parentDir . str_replace(' ', ' ', basename($_FILES['file']['name']));
-echo 'asas==='.$_FILES['file'];
-if (rename($_FILES['file']['tmp_name'], $uploadfilePath)) {
-	echo "success";
-} else {
-	echo "error";
+$target_path = $parentDir;
+
+$tmp_name = $_FILES['upload']['tmp_name'];
+$filename = $_FILES['upload']['name'];
+$target_file = $target_path.$filename;
+$num = $_POST['num'];
+$num_chunks = $_POST['num_chunks'];
+if (!file_exists($target_path)) {
+    mkdir($lockPath, 0777, true);
 }
-//unlink($_FILES['file']['tmp_name']);
+move_uploaded_file($tmp_name, $target_file.$num);
 
-
-// In PHP versions earlier than 4.1.0, $HTTP_POST_FILES should be used instead
-// of $_FILES.
-
-/**
- * Get the given variable from $_REQUEST or from the url
- * @param string $variableName
- * @param mixed $default
- * @return mixed|null
- */
-function getParam($variableName, $default = null) {
-
-	// Was the variable actually part of the request
-	if (array_key_exists($variableName, $_REQUEST)) {
-		return $_REQUEST[$variableName];
-	}
-
-	// Was the variable part of the url
-	$urlParts = explode('/', preg_replace('/\?.+/', '', $_SERVER['REQUEST_URI']));
-	$position = array_search($variableName, $urlParts);
-	if ($position !== false && array_key_exists($position + 1, $urlParts)) {
-		return $urlParts[$position + 1];
-	}
-
-	return $default;
+$lockPath =  $target_path . 'locks/'. $filename . '/';
+if (!file_exists($lockPath)) {
+    mkdir($lockPath, 0777, true);
 }
 
-function ends_with($haystack, $needles) {
-	foreach ((array) $needles as $needle) {
-		if ((string) $needle === substr($haystack, -strlen($needle))) {
-			return true;
+while (true) {
+	$lock = getLock($lockPath, $filename);
+	if ($lock == 1) {
+		break;
+	}
+
+	sleep(10);
+}
+
+// count number of uploaded chunks
+$chunksUploaded = 0;
+for ( $i = 1; $i <= $num_chunks; $i++ ) {
+    if ( file_exists( $target_file.$i ) ) {
+         ++$chunksUploaded;
+    }
+}
+
+// and THAT's what you were asking for
+// when this triggers - that means your chunks are uploaded
+if ($chunksUploaded == $num_chunks) {
+
+    /* here you can reassemble chunks together */
+    for ($i = 1; $i <= $num_chunks; $i++) {
+
+      $file = fopen($target_file.$i, 'rb');
+      $buff = fread($file, filesize($target_file.$i));
+      fclose($file);
+
+      $final = fopen($target_file, 'ab');
+      $write = fwrite($final, $buff);
+      fclose($final);
+
+      unlink($target_file.$i);
+	}
+	rrmdir($lockPath);
+}
+
+if (file_exists($lockPath)) {
+
+	$handle = opendir($lockPath);
+	while (false !== ($entry = readdir($handle))) {
+	  if ($entry !== '.' && $entry !== '..') { // <-- better use strict comparison here
+		rrmdir($lockPath . $entry);
+	  }
+	}
+	closedir($handle);
+}
+
+echo 'success';
+
+function rrmdir($dir) {
+	if (is_dir($dir)) {
+		$objects = scandir($dir);
+		foreach ($objects as $object) {
+			if ($object != "." && $object != "..") {
+				if (is_dir($dir."/".$object))
+					rrmdir($dir."/".$object);
+				else
+					unlink($dir."/".$object);
+			}
 		}
+		rmdir($dir);
 	}
-	return false;
 }
+
+function getLock($lockPath, $filename) {
+	if (is_dir_empty($lockPath) == 1) {
+		$milliseconds = round(microtime(true) * 1000);
+		mkdir($lockPath. '/'.$milliseconds, 0777, true);
+		return isLowestTimestamp($lockPath, $milliseconds);
+	} else {
+		return 0;
+	}
+
+}
+
+function isLowestTimestamp($path, $milliseconds) {
+
+	$dir = opendir($path);
+	while(false != ($file = readdir($dir))) {
+        if(($file != ".") && ($file != "..")) {
+               if ((int)$file < $milliseconds) {
+				   return 0;
+			   }
+        }
+	}
+	return 1;
+}
+
+function is_dir_empty($dir) {
+	$handle = opendir($dir);
+	while (false !== ($entry = readdir($handle))) {
+	  if ($entry !== '.' && $entry !== '..') { // <-- better use strict comparison here
+		closedir($handle); // <-- always clean up! Close the directory stream
+		return 0;
+	  }
+	}
+	closedir($handle); // <-- always clean up! Close the directory stream
+	return 1;
+  }
 
 ?>
